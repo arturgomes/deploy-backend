8c1e3da6c9ec4db9d6eba5208b7d1eb7df54ccec
Artur Oliveira Gomes <artur@MacBook-Pro.local>
2020-07-29 07:25:21 -0400

    social login - 29072020

--- src/services/api.js
+++ src/services/api.js
@@ -4,9 +4,9 @@ import { getToken } from "./auth";
 
 const api = axios.create({
   // baseURL: 'http://localhost:3000',
-  baseURL: 'https://api.couponfeed.co',
+  // baseURL: 'https://api.couponfeed.co',
   withCredentials: true
-  // baseURL: 'https://cors-anywhere.herokuapp.com/https://api.couponfeed.co',
+  baseURL: 'https://cors-anywhere.herokuapp.com/https://api.couponfeed.co',
 });
 
 
