2e07b381a7790a77642287dabcdf37342b234555
Artur Oliveira Gomes <artur@MacBook-Pro.local>
2020-07-29 07:49:12 -0400

    social login - 29072020

--- src/layouts/Login.js
+++ src/layouts/Login.js
@@ -76,7 +76,7 @@ export default class Login extends Component {
   // }
   async componentDidMount() {
 
-      await api.get("https://api.couponfeed.co/auth/success")
+      await api.get("/auth/success")
       // fetch("https://api.couponfeed.co/auth/success", {
       //   method: "GET",
       //   credentials: "include",
