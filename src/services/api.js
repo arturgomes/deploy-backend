5da27cdef6c86e36bb7ce575429abb1e32eae5e3
Artur Oliveira Gomes <>
2020-07-30 15:12:08 -0400

    social login - 30072020

--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,7 @@ import { getToken } from "./auth";
 const api = axios.create({
   // baseURL: 'http://localhost:3000',
   baseURL: 'https://api.couponfeed.co',
-  // withCredentials: true,
+  withCredentials: true,
   
   // baseURL: 'https://cors-anywhere.herokuapp.com/https://api.couponfeed.co',
 });
