7972201ed08bf4246fd9f8490625a32c9fddede4
Artur Oliveira Gomes <>
2020-07-31 13:26:41 -0400

    social login - 31072020

--- src/services/api.js
+++ src/services/api.js
@@ -6,14 +6,14 @@ const api = axios.create({
   // baseURL: 'http://localhost:3000',
   baseURL: 'https://api.couponfeed.co',
   withCredentials: true,
-  crossorigin:true
-  // headers: {                  
-  //   "Access-Control-Allow-Origin": "http://localhost:3001",
-  //   // "Access-Control-Allow-Origin": "https://couponfeed.co/",
+  // crossorigin:true
+  headers: {                  
+    "Access-Control-Allow-Origin": "http://localhost:3001",
+    // "Access-Control-Allow-Origin": "https://couponfeed.co/",
   //   "Access-Control-Allow-Headers": "Authorization", 
   //   "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE" ,
   //   "Content-Type": "application/json;charset=UTF-8"                   
-  // },
+  },
   
   // baseURL: 'https://cors-anywhere.herokuapp.com/https://api.couponfeed.co',
 });
